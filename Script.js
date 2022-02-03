804bc9d7eddc68770025afca2dc48d5e117abc21
Meghan Seger <meghanbmckinney@gmail.com>
2022-02-02 21:14:42 -0600

    Started adding audio

---
+++ Script.js
@@ -0,0 +1,12 @@
+Class AudioController {
+    constructor () {
+        this.flipSound = new Audio(Assets/Audio/.wav);
+        this.matchSound = new Audio(Assets/Audio/.wav);
+        this.greatjobSound = new Audio(Assets/Audio/.wav);
+        this.gaveOverSound = new Audio(Assets/Audio/.wav);
+
+    }
+}
+flip() {
+    
+}
